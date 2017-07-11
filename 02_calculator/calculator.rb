321b7c4aeb74d640b14ec12dfc44da50533f1326
Durev <vincent.durewski@gmail.com>
2017-07-11 16:34:30 +0200

    exo calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -10,3 +10,16 @@ end
 def sum(tab)
 	tab.inject(0, :+)
 end
+
+def multiply(n, *m)
+	n*m.inject(:*)
+end
+
+def power(n, m)
+	n**m
+end
+
+def factorial(n)
+	n = 1 if n == 0 
+	else (1..n).inject(:*)
+end
