3cdf0eb2416741cbfd33445f62093d6220692f59
ArthurL <alanter.pro@gmail.com>
2017-07-11 16:01:19 +0200

    Bonus calcul

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,9 +79,13 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    expect (multiply(6,8)).to eq(48)
+  end
 
-  it "multiplies several numbers"
+  it "multiplies several numbers" do
+    expect (multiply(6,8,5)).to eq(240)
+  end
   
 end
 
